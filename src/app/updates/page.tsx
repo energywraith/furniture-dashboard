25909ac4f107ce77ab8857a8d7f3a1b8c393290e
energywraith <energywraith@gmail.com>
2023-12-11 16:24:41 +0100

    feat: Products, categories, orders, updates routes

---
+++ src/app/updates/page.tsx
@@ -0,0 +1,7 @@
+export default function Updates() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-center p-24">
+      <h1 className="text-3xl">Aktualności</h1>
+    </main>
+  );
+}
