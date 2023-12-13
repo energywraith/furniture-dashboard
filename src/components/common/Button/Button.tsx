f1afb83f5b3f668fd7f9e158d51342e2721d053f
energywraith <energywraith@gmail.com>
2023-12-13 17:30:36 +0100

    fix: Added small, medium, large buttons variants

--- src/components/common/Button/Button.tsx
+++ src/components/common/Button/Button.tsx
@@ -1,19 +1,24 @@
 import { IconProps } from "@/components/icons";
 import { ReactNode } from "react";
+import { variants } from "./variants";
 
 interface ButtonProps {
+  variant?: keyof typeof variants;
   LeftAttachmentIcon?: React.FC<IconProps>;
   RightAttachmentIcon?: React.FC<IconProps>;
   children: ReactNode;
 }
 
 const Button = ({
+  variant = "medium",
   LeftAttachmentIcon,
   RightAttachmentIcon,
   children,
 }: ButtonProps) => {
   return (
-    <button className="border border-neutral-700 rounded-md px-3 py-2.5 text-body-s font-semibold flex items-center gap-x-1.5">
+    <button
+      className={`border border-neutral-700 rounded-md text-body-s font-semibold flex items-center gap-x-1.5 ${variants[variant]}`}
+    >
       {LeftAttachmentIcon && <LeftAttachmentIcon className="h-4" />}
       {children}
       {RightAttachmentIcon && <RightAttachmentIcon className="h-4" />}
