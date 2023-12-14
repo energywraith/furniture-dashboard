9e7f820643f18c9adb46535f57400761f5687f9e
energywraith <energywraith@gmail.com>
2023-12-14 18:02:32 +0100

    feat: Dropdown menu

--- src/components/templates/DropdownMenu/DropdownMenu.tsx
+++ src/components/templates/DropdownMenu/DropdownMenu.tsx
@@ -1,22 +1,65 @@
-import { Tooltip } from "@/components/common/Tooltip";
-import { ChevronIcon, IconProps } from "@/components/icons";
-import { HTMLProps } from "react";
+import { CloseIcon, MoreIcon } from "@/components/icons";
+import { useRef, useState } from "react";
+import { DropdownMenuTransition } from "./DropdownMenuTransition";
+import { DropdownMenuOption } from "./DropdownMenuOption";
+import { useClickOutside } from "@/hooks/useClickOutside";
 
-interface DropdownMenuProps extends HTMLProps<HTMLDivElement> {
-  Icon: React.FC<IconProps>;
-  tooltipText: string;
+interface DropdownMenuProps {
+  options: DropdownMenuOption[];
 }
 
-const DropdownMenu = ({ Icon, tooltipText }: DropdownMenuProps) => {
+const DropdownMenu = ({ options }: DropdownMenuProps) => {
+  const menuRef = useRef(null);
+  const [isMenuVisible, setIsMenuVisible] = useState(false);
+
+  const MenuIcon = isMenuVisible ? CloseIcon : MoreIcon;
+
+  const toggleMenu = () => {
+    setIsMenuVisible((value) => !value);
+  };
+
+  const onOptionClick = (callback: () => void) => {
+    setIsMenuVisible(false);
+    callback();
+  };
+
+  useClickOutside({
+    ref: menuRef,
+    onClickOutside: () => {
+      setIsMenuVisible((isVisible) => {
+        if (isVisible) {
+          return !isVisible;
+        }
+
+        return isVisible;
+      });
+    },
+  });
+
   return (
-    <button className="group border-2 border-transparent hover:border-info-950 rounded-[40px] p-0.5 hover:text-info-950">
-      <div className="group-hover:bg-info-100 w-full h-full rounded-[40px] p-3 flex items-center gap-x-1">
-        <Tooltip text={tooltipText}>
-          <Icon className="w-6 h-6" />
-        </Tooltip>
-        <ChevronIcon className="w-4 h-4" />
-      </div>
-    </button>
+    <div ref={menuRef} className="relative">
+      <button
+        onClick={toggleMenu}
+        className={` rounded-full p-2 ${
+          !isMenuVisible ? "hover:bg-info-100 hover:text-info-950" : ""
+        }`}
+      >
+        <MenuIcon className="w-6 h-6" />
+      </button>
+      <DropdownMenuTransition active={isMenuVisible}>
+        <div className="mt-3 w-56 rounded-md bg-white focus:outline-none py-1 shadow-xs border border-neutral-700">
+          <ul>
+            {options.map((option) => (
+              <DropdownMenuOption
+                key={option.id}
+                {...option}
+                onOptionClickHandle={onOptionClick}
+              />
+            ))}
+          </ul>
+        </div>
+      </DropdownMenuTransition>
+    </div>
   );
 };
 
