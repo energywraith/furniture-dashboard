fae490c192864cc35d9a064369d29b87f5c6e8be
energywraith <energywraith@gmail.com>
2023-12-14 18:02:39 +0100

    feat: Added dropdown menu to Card

--- src/components/common/Card/Card.tsx
+++ src/components/common/Card/Card.tsx
@@ -1,20 +1,34 @@
-import { MoreIcon } from "@/components/icons";
 import { HTMLProps } from "react";
+import {
+  DropdownMenu,
+  DropdownMenuOption,
+} from "@/components/templates/DropdownMenu";
 
 interface CardProps extends HTMLProps<HTMLDivElement> {
   title: string;
+  menuOptions?: DropdownMenuOption[];
 }
 
-const Card = ({ title, className, children, ...props }: CardProps) => (
+const Card = ({
+  title,
+  className,
+  children,
+  menuOptions,
+  ...props
+}: CardProps) => (
   <div
     {...props}
     className={`shadow-xs border border-neutral-600 rounded-xl w-fit ${className}`}
   >
-    <div className="flex justify-between px-6 py-4 border-b border-neutral-600">
+    <div
+      className={`flex justify-between border-b border-neutral-600 items-center ${
+        menuOptions ? "py-2 pl-6 pr-4" : "py-4 px-6"
+      }`}
+    >
       <h2 className="text-body-m xl:text-body-l font-medium text-gray-700">
         {title}
       </h2>
-      <MoreIcon className="w-6 h-6" />
+      {menuOptions && <DropdownMenu options={menuOptions} />}
     </div>
     <div>{children}</div>
   </div>
