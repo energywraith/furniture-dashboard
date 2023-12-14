2b81c3b7a41ea48bab8a91217fa3d236e5b5909b
energywraith <energywraith@gmail.com>
2023-12-14 01:17:00 +0100

    feat: ClientsTable

--- src/components/templates/Page/Page.tsx
+++ src/components/templates/Page/Page.tsx
@@ -5,7 +5,11 @@ interface PageProps {
 }
 
 const Page = ({ children }: PageProps) => {
-  return <main className="container h-full pt-7 md:pt-10">{children}</main>;
+  return (
+    <main className="container h-full pt-7 pb-16 md:pt-10 md:pb-20">
+      {children}
+    </main>
+  );
 };
 
 export { Page };
